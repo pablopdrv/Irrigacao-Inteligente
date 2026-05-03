d8cd8fa4f37bfa85b50806aff5c216a103f2aa11
Pablo Diego <pablodiego5000@gmail.com>
2026-05-03 19:13:13 -0300

    adicionando porta dinamica

--- server.js
+++ server.js
@@ -87,6 +87,8 @@ app.get("/plantas", (req, res) => {
   res.json(plantas);
 });
 
-app.listen(3000, () => {
-  console.log("Rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
 });
